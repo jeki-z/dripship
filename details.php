38a3d029c378bf6841845cbf05a231d380258be0
patsac <patar.isac@gmail.com>
2021-12-17 08:06:04 +0700

    format files

--- details.php
+++ details.php
@@ -1,76 +1,79 @@
-<?php include("config.php");
-    $id = $_GET["id"];
-	$q_ship = pg_query("SELECT * FROM shipment WHERE id=$id");
-	$ship = pg_fetch_array($q_ship);
-	$order_id = $ship["order_id"];
-	$q_order = pg_query("SELECT * FROM orders WHERE id=$order_id");
-	$order = pg_fetch_array($q_order);
-	$sender_id = $order["sender_id"];
-	$recip_id = $order["recipient_id"];
-	$item_type = $order["item_type"];
-	$item_qty = $order["item_qty"];
-	$order_date = $order["order_date"];
-	$ship_type = $order["shipment_type"];
-	$ship_status = $ship["shipment_status"];
-	$ship_date = $ship["shipment_date"];
-	$courier_id = $ship["courier_id"];
-	$q_sender = pg_query("SELECT * FROM customers WHERE id=$sender_id");
-	$sender = pg_fetch_array($q_sender);
-	$sender_name = $sender["name"];
-	$sender_addr = $sender["address"];
-	$sender_pcode = $sender["pcode"];
-	$sender_pnumber = $sender["phone_number"];
-	$q_recip = pg_query("SELECT * FROM customers WHERE id=$recip_id");
-	$recip = pg_fetch_array($q_recip);
-	$recip_name = $recip["name"];
-	$recip_addr = $recip["address"];
-	$recip_pcode = $recip["pcode"];
-	$recip_pnumber = $recip["phone_number"];
-	$q_courier = pg_query("SELECT * FROM courier WHERE id=$courier_id");
-	$courier = pg_fetch_array($q_courier);
-	$courier_name = $courier["name"];
+<?php
+   include("config.php");
+   $id = $_GET["id"];
+   $q_ship = pg_query("SELECT * FROM shipment WHERE id=$id");
+   $ship = pg_fetch_array($q_ship);
+   $order_id = $ship["order_id"];
+   $q_order = pg_query("SELECT * FROM orders WHERE id=$order_id");
+   $order = pg_fetch_array($q_order);
+   $sender_id = $order["sender_id"];
+   $recip_id = $order["recipient_id"];
+   $item_type = $order["item_type"];
+   $item_qty = $order["item_qty"];
+   $order_date = $order["order_date"];
+   $ship_type = $order["shipment_type"];
+   $ship_status = $ship["shipment_status"];
+   $ship_date = $ship["shipment_date"];
+   $courier_id = $ship["courier_id"];
+   $q_sender = pg_query("SELECT * FROM customers WHERE id=$sender_id");
+   $sender = pg_fetch_array($q_sender);
+   $sender_name = $sender["name"];
+   $sender_addr = $sender["address"];
+   $sender_pcode = $sender["pcode"];
+   $sender_pnumber = $sender["phone_number"];
+   $q_recip = pg_query("SELECT * FROM customers WHERE id=$recip_id");
+   $recip = pg_fetch_array($q_recip);
+   $recip_name = $recip["name"];
+   $recip_addr = $recip["address"];
+   $recip_pcode = $recip["pcode"];
+   $recip_pnumber = $recip["phone_number"];
+   $q_courier = pg_query("SELECT * FROM courier WHERE id=$courier_id");
+   $courier = pg_fetch_array($q_courier);
+   $courier_name = $courier["name"];
 ?>
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <!-- Required meta tags -->
-        <meta charset="utf-8" />
-        <meta name="viewport" content="width=device-width, initial-scale=1" />
-        <!-- Bootstrap CSS -->
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
-        <link rel="stylesheet" href="styleDetail.css" />
-        <title>DripShip | Data Details</title>
-    </head>
-	<style>
-		.tab {
-			padding-left: 2px;
-		}
-	</style>
-    <body>
-        <!-- Navbar -->
-		<nav class="navbar navbar-expand-lg navbar-dark bg-secondary shadow fixed-top">
-			<div class="container-fluid">
-				<a class="navbar-brand" href="/dripship">DripShip!</a>
-				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-				<span class="navbar-toggler-icon"></span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarNav">
-					<ul class="navbar-nav">
-						<li class="nav-item">
-							<a class="nav-link active" aria-current="page" href="#home">Home</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="#create">Create Order</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link" href="shipment.php">Shipment</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
-      <section id="create">
+
+<head>
+   <!-- Required meta tags -->
+   <meta charset="utf-8" />
+   <meta name="viewport" content="width=device-width, initial-scale=1" />
+   <!-- Bootstrap CSS -->
+   <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+   <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css" />
+   <link rel="stylesheet" href="styleDetail.css" />
+   <title>DripShip | Data Details</title>
+</head>
+<style>
+   .tab {
+      padding-left: 2px;
+   }
+</style>
+
+<body>
+   <!-- Navbar -->
+   <nav class="navbar navbar-expand-lg navbar-dark bg-secondary shadow fixed-top">
+      <div class="container-fluid">
+         <a class="navbar-brand" href="/dripship">DripShip!</a>
+         <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+         </button>
+         <div class="collapse navbar-collapse" id="navbarNav">
+            <ul class="navbar-nav">
+               <li class="nav-item">
+                  <a class="nav-link active" aria-current="page" href="#home">Home</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="#create">Create Order</a>
+               </li>
+               <li class="nav-item">
+                  <a class="nav-link" href="shipment.php">Shipment</a>
+               </li>
+            </ul>
+         </div>
+      </div>
+   </nav>
+   <section id="create">
       <div class="cointainer text-center text-white mb-5">
          <h2>Details</h2>
       </div>
@@ -90,7 +93,7 @@
                      <td>: <?= $sender_name ?></td>
                   </tr>
                   <tr>
-                     <th>Address</th>	
+                     <th>Address</th>
                      <td>: <?= $sender_addr ?></td>
                   </tr>
                   <tr>
@@ -101,9 +104,11 @@
                      <th>Phone</th>
                      <td>: <?= $sender_pnumber ?></td>
                   </tr>
-				  <tr>
-                    <th> </th>
-                    <td><pre>                                                                  </pre></td>
+                  <tr>
+                     <th> </th>
+                     <td>
+                        <pre>                                                                  </pre>
+                     </td>
                   </tr>
                </table>
             </div>
@@ -132,9 +137,11 @@
                      <th>Phone</th>
                      <td>: <?= $recip_pnumber ?></td>
                   </tr>
-				  <tr>
-                    <th> </th>
-                    <td><pre>                                                                  </pre></td>
+                  <tr>
+                     <th> </th>
+                     <td>
+                        <pre>                                                                  </pre>
+                     </td>
                   </tr>
                </table>
             </div>
@@ -160,9 +167,11 @@
                      <th>Item Quantity</th>
                      <td>: <?= $item_qty ?></td>
                   </tr>
-				  <tr>
-                    <th> </th>
-                    <td><pre>                                                                  </pre></td>
+                  <tr>
+                     <th> </th>
+                     <td>
+                        <pre>                                                                  </pre>
+                     </td>
                   </tr>
                </table>
             </div>
@@ -196,23 +205,22 @@
                      <td>: <?= $ship_status ?></td>
                   </tr>
                   <tr>
-                    <th> </th>
-                    <td><pre>                                                                  </pre></td>
+                     <th> </th>
+                     <td>
+                        <pre>                                                                  </pre>
+                     </td>
                   </tr>
                </table>
             </div>
-			<div class="col">
-				<a href="shipment.php" class="btn btn-primary">Back</a>
-			</div>
+            <div class="col">
+               <a href="shipment.php" class="btn btn-primary">Back</a>
+            </div>
          </div>
       </div>
-        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-            <path
-                fill="#3a404b"
-                fill-opacity="1"
-                d="M0,224L40,202.7C80,181,160,139,240,128C320,117,400,139,480,149.3C560,160,640,160,720,154.7C800,149,880,139,960,138.7C1040,139,1120,149,1200,160C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-                ></path>
-        </svg>
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    </body>
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+         <path fill="#3a404b" fill-opacity="1" d="M0,224L40,202.7C80,181,160,139,240,128C320,117,400,139,480,149.3C560,160,640,160,720,154.7C800,149,880,139,960,138.7C1040,139,1120,149,1200,160C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+      </svg>
+      <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+</body>
+
 </html>
--- index.php
+++ index.php
@@ -2,146 +2,141 @@
 
 <!DOCTYPE html>
 <html lang="en">
-  <head>
-    <!-- Required meta tags -->
-    <meta charset="utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <!-- Bootstrap CSS -->
-    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
-    <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" />
-    <!--My CSS-->
-    <link rel="stylesheet" href="style.css" />
+<head>
+  <!-- Required meta tags -->
+  <meta charset="utf-8" />
+  <meta name="viewport" content="width=device-width, initial-scale=1" />
 
-    <title>DripShip</title>
-  </head>
-  <body id="home">
-    <!-- Navbar -->
-    <nav class="navbar navbar-expand-lg navbar-dark bg-secondary shadow fixed-top">
-      <div class="container-fluid">
-        <a class="navbar-brand" href="/dripship">DripShip!</a>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-          <span class="navbar-toggler-icon"></span>
-        </button>
-        <div class="collapse navbar-collapse" id="navbarNav">
-          <ul class="navbar-nav">
-            <li class="nav-item">
-              <a class="nav-link active" aria-current="page" href="#home">Home</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="#create">Create Order</a>
-            </li>
-            <li class="nav-item">
-              <a class="nav-link" href="shipment.php">Shipment</a>
-            </li>
-          </ul>
-        </div>
+  <!-- Bootstrap CSS -->
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous" />
+  <link rel="stylesheet" href="https://cdn.datatables.net/1.10.19/css/dataTables.bootstrap4.min.css" />
+  <!--My CSS-->
+  <link rel="stylesheet" href="style.css" />
+
+  <title>DripShip</title>
+</head>
+
+<body id="home">
+  <!-- Navbar -->
+  <nav class="navbar navbar-expand-lg navbar-dark bg-secondary shadow fixed-top">
+    <div class="container-fluid">
+      <a class="navbar-brand" href="/dripship">DripShip!</a>
+      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarNav">
+        <ul class="navbar-nav">
+          <li class="nav-item">
+            <a class="nav-link active" aria-current="page" href="#home">Home</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="#create">Create Order</a>
+          </li>
+          <li class="nav-item">
+            <a class="nav-link" href="shipment.php">Shipment</a>
+          </li>
+        </ul>
       </div>
-    </nav>
-    <!-- Jumbotron-->
-    <section class="jumbotron text-center text-white">
-      <img src="img/logo1.png" alt="DripShip_logo" width="175" />
-      <h5 class="display-4">Where Your Fashion Feel Safe</h5>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#ffff"
-          fill-opacity="1"
-          d="M0,160L40,149.3C80,139,160,117,240,128C320,139,400,181,480,197.3C560,213,640,203,720,186.7C800,171,880,149,960,128C1040,107,1120,85,1200,90.7C1280,96,1360,128,1400,144L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-        ></path>
-      </svg>
-    </section>
+    </div>
+  </nav>
+  <!-- Jumbotron-->
+  <section class="jumbotron text-center text-white">
+    <img src="img/logo1.png" alt="DripShip_logo" width="175" />
+    <h5 class="display-4">Where Your Fashion Feel Safe</h5>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#ffff" fill-opacity="1" d="M0,160L40,149.3C80,139,160,117,240,128C320,139,400,181,480,197.3C560,213,640,203,720,186.7C800,171,880,149,960,128C1040,107,1120,85,1200,90.7C1280,96,1360,128,1400,144L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+    </svg>
+  </section>
 
-    <!--Create-->
-    <section id="create">
-      <form action="add_order.php" method="POST">
-        <div class="cointainer text-center">
-          <h3>Customer Data</h3>
-        </div>
-        <div class="container">
-          <div class="row">
+  <!--Create-->
+  <section id="create">
+    <form action="add_order.php" method="POST">
+      <div class="cointainer text-center">
+        <h3>Customer Data</h3>
+      </div>
+      <div class="container">
+        <div class="row">
+          <div class="col">
+            <div class="container text-center"><b>Sender</b></div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="s-name" class="form-label">Name</label>
+              <input class="form-control" type="text" name="s-name" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="s-address" class="form-label">Address</label>
+              <input class="form-control" type="text" name="s-address" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="s-pcode" class="form-label">Postal Code</label>
+              <input class="form-control" type="text" name="s-pcode" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="s-phone" class="form-label">Phone Number</label>
+              <input class="form-control" type="text" name="s-phone" placeholder="Default input" />
+            </div>
+          </div>
+          <div class="col">
+            <div class="container text-center"><b>Recipient</b></div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="r-name" class="form-label">Name</label>
+              <input class="form-control" type="text" name="r-name" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="r-address" class="form-label">Address</label>
+              <input class="form-control" type="text" name="r-address" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="r-pcode" class="form-label">Postal Code</label>
+              <input class="form-control" type="text" name="r-pcode" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="r-phone" class="form-label">Phone Number</label>
+              <input class="form-control" type="text" name="r-phone" placeholder="Default input" />
+            </div>
+            <br>
+          </div>
+          <div class="cointainer text-center">
+            <h3>Order Data</h3>
+          </div>
+          <div class="row text-left">
+            <div class="mb-3 ms-5 me-5">
+              <label for="item-type" class="form-label">Item Type</label>
+              <input class="form-control" type="text" name="item-type" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="item-qty" class="form-label">Item Quantity</label>
+              <input class="form-control" type="number" name="item-qty" placeholder="Default input" />
+            </div>
+            <div class="mb-3 ms-5 me-5">
+              <label for="ship-type" class="form-label">Shipment Type</label>
+              <select class="form-select" name="ship-type" aria-label="Regular">
+                <option value="Regular" selected>Regular</option>
+                <option value="Premium">Premium</option>
+              </select>
+            </div>
+          </div>
+          <div class="row text-left">
             <div class="col">
-              <div class="container text-center"><b>Sender</b></div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="s-name" class="form-label">Name</label>
-                <input class="form-control" type="text" name="s-name" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="s-address" class="form-label">Address</label>
-                <input class="form-control" type="text" name="s-address" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="s-pcode" class="form-label">Postal Code</label>
-                <input class="form-control" type="text" name="s-pcode" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="s-phone" class="form-label">Phone Number</label>
-                <input class="form-control" type="text" name="s-phone" placeholder="Default input" />
-              </div>
-            </div>
-            <div class="col">
-              <div class="container text-center"><b>Recipient</b></div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="r-name" class="form-label">Name</label>
-                <input class="form-control" type="text" name="r-name" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="r-address" class="form-label">Address</label>
-                <input class="form-control" type="text" name="r-address" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="r-pcode" class="form-label">Postal Code</label>
-                <input class="form-control" type="text" name="r-pcode" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="r-phone" class="form-label">Phone Number</label>
-                <input class="form-control" type="text" name="r-phone" placeholder="Default input" />
-              </div>
-              <br>
-            </div>
-            <div class="cointainer text-center">
-              <h3>Order Data</h3>
-            </div>
-            <div class="row text-left">
-              <div class="mb-3 ms-5 me-5">
-                <label for="item-type" class="form-label">Item Type</label>
-                <input class="form-control" type="text" name="item-type" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="item-qty" class="form-label">Item Quantity</label>
-                <input class="form-control" type="number" name="item-qty" placeholder="Default input" />
-              </div>
-              <div class="mb-3 ms-5 me-5">
-                <label for="ship-type" class="form-label">Shipment Type</label>
-                <select class="form-select" name="ship-type" aria-label="Regular">
-                  <option value="Regular" selected>Regular</option>
-                  <option value="Premium">Premium</option>
-                </select>
-              </div>
-            </div>
-            <div class="row text-left">
-              <div class="col">
-                <button type="submit" name="submit" value="submit" class="btn btn-primary ms-5">Submit</button>
-              </div>
+              <button type="submit" name="submit" value="submit" class="btn btn-primary ms-5">Submit</button>
             </div>
           </div>
         </div>
-      </form>
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
-        <path
-          fill="#3a404b"
-          fill-opacity="1"
-          d="M0,224L40,202.7C80,181,160,139,240,128C320,117,400,139,480,149.3C560,160,640,160,720,154.7C800,149,880,139,960,138.7C1040,139,1120,149,1200,160C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
-        ></path>
-      </svg>
-    </section>
+      </div>
+    </form>
+    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320">
+      <path fill="#3a404b" fill-opacity="1" d="M0,224L40,202.7C80,181,160,139,240,128C320,117,400,139,480,149.3C560,160,640,160,720,154.7C800,149,880,139,960,138.7C1040,139,1120,149,1200,160C1280,171,1360,181,1400,186.7L1440,192L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"></path>
+    </svg>
+  </section>
 
 
 
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
-    <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
-    <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
-    <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
-  </body>
+  <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js"></script>
+  <script src="https://code.jquery.com/jquery-3.3.1.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/jquery.dataTables.min.js"></script>
+  <script src="https://cdn.datatables.net/1.10.19/js/dataTables.bootstrap4.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.1.0/jquery.min.js" type="text/javascript"></script>
+</body>
+
 </html>
