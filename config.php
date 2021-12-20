14c62a62158d280eef890ddbc85cfeb66ad2e75b
patarisac <patar.isac@gmail.com>
2021-12-20 13:21:45 +0700

    update config.php

--- config.php
+++ config.php
@@ -1,5 +1,5 @@
 <?php
-$db=pg_connect('host=localhost dbname=dripship user=postgres password=patar');
+$db=pg_connect('host=localhost dbname=dripship user=postgres password=password');
 if( !$db ){
 die("Gagal terhubung dengan database: " . pg_connect_error());
 }
